82ddbe8c486942c224af16291a358c5029023808
aguskuster <61338886+aguskuster@users.noreply.github.com>
2020-08-15 19:46:49 -0300

    Update logOut.js

--- js/logOut.js
+++ js/logOut.js
@@ -3,7 +3,7 @@
         var auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function() {
             alert('User signed out.');
-            location.href = "aguskuster.github.io/JAP-ecommerce/index.html"
+            location.href = "https://aguskuster.github.io/JAP-ecommerce/index.html"
         });
     }
 
